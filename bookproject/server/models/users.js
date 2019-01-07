ef38f3629e3946d4a588ca031ad7c888b785117e
Vishal Singh <vishal.singh@mountblue.io>
2019-01-07 22:26:29 +0530

    added joi Validation

--- bookproject/server/models/users.js
+++ bookproject/server/models/users.js
@@ -1,56 +1,96 @@
-let mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Joi = require('joi');
 let books = require('./book')
 // let check = require('../books.json')
-
+//console.log(books.find({'isbn':"9781449325862"}))
 
 let userSchema = mongoose.Schema({
     "user": String,
     "password": String,
-    "want_to_read": Array,
+    "want-to-read": Array,
     "read": Array,
     "reading": Array
 
 })
 
-let users = module.exports = mongoose.model('users', userSchema)
+const joiSchema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    isbn: Joi.number()
+    // id:Joi.string().alphanum().min(3).max(70).required()
+})
+// let validation = (newUser,isbn)=>{
+//     return Joi.validate({
+//         username: newUser
+//     }, joiSchema)
+// }
 
+
+let users = module.exports = mongoose.model('users', userSchema)
+// users.find().then(data=>console.log(data))
 
 //  users.insertMany(
 //      [
 //      {
 //      "user":"Vishal",
 //      "password":"vbnm",
-//      "want_to_read":[{"isbn": "9781449337711","title": "Designing Evolvable Web APIs with ASP.NET"}],
-//      "read":[{"isbn": "9781449325862","title": "Git Pocket Guide"}],
-//       "reading":[{"isbn": "9781491950296","title": "Programming JavaScript Applications"}]
+//      "want-to-read":[{"isbn": "9781449337711"}],
+//      "read":[{"isbn": "9781449325862"}],
+//       "reading":[{"isbn": "9781491950296"}]
 //  },
 // {
 //     "user":"Harish",
 //     "password":"mnvb",
-//     "want_to_read":[{"isbn": "9781593277574","title": "Understanding ECMAScript 6"}],
-//     "read":[{"isbn": "9781491950296","title": "Programming JavaScript Applications"}],
-//     "reading":[{"isbn": "9781449365035","title": "Speaking JavaScript"}]
+//     "want-to-read":[{"isbn": "9781593277574"}],
+//     "read":[{"isbn": "9781491950296"}],
+//     "reading":[{"isbn": "9781449365035"}]
 // }
 // ]
 // )
 
-//get all users data
-// module.exports.getUsers = (callbacks) => {
-//     users.find(callbacks)
-// }
+// get all users data
+module.exports.getUsers = (callbacks) => {
+    users.find(callbacks)
+}
 
 //get all users by id
-module.exports.getUserId = (id, callbacks) => {
-    users.findById(id, callbacks)
+module.exports.getUserId = (_user, callbacks) => {
+    let validation = Joi.validate({
+        username: _user
+    }, joiSchema)
+    if(validation.error===null){
+        users.findOne({user:_user}).then(data=>{
+            console.log(data)
+            if(data===null){
+                callbacks('User name dont exist',400)
+            }else callbacks(data,200)
+        })
+    }else callbacks('Invalid Request',400)
 }
 
 //add new user
 module.exports.addUser = (newUser, callbacks) => {
-    users.create(newUser, callbacks)
+    let validation = Joi.validate({
+        username: newUser
+    }, joiSchema)
+    console.log(validation)
+    if(validation.error=== null){
+        users.findOne({
+            user: newUser
+        }).then(data => {
+            console.log(data)
+            if(data === null){
+                users.create({
+                    user: newUser
+                }).then(data =>{
+                    callbacks(data,201)
+                }) 
+            }else callbacks('Username already exists', 400)
+        })
+    }else callbacks(`Invalid Username `,400)
 }
 
 //get method
-module.exports.userBooks = (checkuser,checkData ,callbacks) => {
+module.exports.userBooks = (checkuser, checkData, callbacks) => {
     let check = {
         user: checkuser
     }
@@ -58,42 +98,180 @@ module.exports.userBooks = (checkuser,checkData ,callbacks) => {
 }
 
 //update method
-module.exports.userBooksUpdate = (checkuser, _isbn,checkData ,callbacks) => {
-    let query = {
-        user: checkuser
-    }
-    books.find({
-        isbn: _isbn
-    }).then((err, data) => {
-        console.log(data)
-        users.findOneAndUpdate(query, {
-            $push: {
-                [checkData]: {
-                    title: data[0].title,
-                    isbn: data[0].isbn
-                }
-            }
-        }, callbacks)
-    })
 
+
+module.exports.userBooksUpdate = (_user,_isbn,checkData ,callbacks) => {
+    let validation = Joi.validate({
+        username: _user,
+        isbn: _isbn
+    }, joiSchema)
+
+    if(validation.error===null){
+        books.findOne({
+            isbn: _isbn
+          }).then(data => {
+            console.log(data)
+           if(data===null){
+              callbacks('Galat hai',400)
+           }else{
+            users.findOne({
+                user: _user
+              }).then(userData => {
+                let isIsbnExist = userData[checkData].some(element => element.isbn === _isbn)
+          
+                if (!isIsbnExist) {
+                  userData[checkData].push({
+                    isbn: data.isbn
+                  })
+                  userData.save();
+                  callbacks('Sucess',201)
+                } else callbacks("Already present",400);
+              })
+           }
+        
+        
+          })
+    }else callbacks('Wrong Entry',400)
 }
+
+
+
+
+
+// module.exports.userBooksUpdate = (checkuser, _isbn,checkData ,callbacks) => {
+//     let query = {
+//         user: checkuser
+//     }
+//     books.find({
+//         isbn: _isbn
+//     },(err, data) => {
+//         console.log(data)
+//         users.findOneAndUpdate(query, {
+//             $push: {
+//                 [checkData]: {
+//                     title: data[0].title,
+//                     isbn: data[0].isbn
+//                 }
+//             }
+//         }, callbacks)
+//     })
+
+// }
+
+
+
+
+
 //delete method
 
-module.exports.userBooksDelete = (id, _isbn,checkData, callbacks) => {
-    let query = {
-        _id: id
-    }
-    books.find({
+module.exports.userBooksDelete = async (_user, _isbn, checkData) => {
+
+
+   let validation = Joi.validate({
+        username: _user,
         isbn: _isbn
-    }).then((err, data) => {
-        console.log(data)
-        users.findOneAndUpdate(query, {
-            $pull: {
-                [checkData]: {
-                    title: data[0].title,
-                    isbn: data[0].isbn
+    }, joiSchema)
+    // console.log('fgbrt',validation)
+
+try{    if (validation.error === null) {
+        let userData =  await users.findOne({
+            user: _user
+        });
+        
+       
+            let length = userData[checkData].length;
+
+            for (let i in userData[checkData]) {
+                if (userData[checkData][i].isbn === _isbn) {
+                    userData[checkData].splice(i, 1)
+                    if (i >= userData[checkData].length) break
+
                 }
             }
-        }, callbacks)
-    })
+
+            await userData.save();
+            if (userData[checkData].length !== length) {
+
+                return userData[checkData]
+            } else return "don't exist";
+
+
+
+
+
+
+        // }).then(userData => {
+        //     console.log(userData)
+
+        //     let length = userData[checkData].length;
+
+        //     for (let i in userData[checkData]) {
+        //         if (userData[checkData][i].isbn === _isbn) {
+        //             userData[checkData].splice(i, 1)
+        //             if (i >= userData[checkData].length) break
+
+        //         }
+        //     }
+
+        //     userData.save();
+        //     if (userData[checkData].length !== length) {
+
+        //         callbacks(userData[checkData], 201)
+        //     } else callbacks("don't exist", 400)
+
+        // })
+    } else return 'Galat hai bhai Request';
+}catch(err){
+    //  console.log(err)
+     return err
 }
+
+
+}
+
+// module.exports.userBooksDelete = (_user, _isbn, checkData, callbacks) => {
+
+    // let query = {
+    //     id: _id
+    // }
+
+    //    users.findOne(query).then(userData=>{
+    //     //    console.log(userData)
+
+    //    let length = userData[checkData].length;
+
+    //         for(let i in userData[checkData]){
+    //             if(userData[checkData][i].isbn===_isbn){
+    //                 userData[checkData].splice(i,1)
+    //                 if(i>=userData[checkData].length) break
+
+    //             }
+    //         }
+
+    //         userData.save();
+    //          if(userData[checkData].length===length){
+
+    //              callbacks(false)
+    //          }else{
+    //             callbacks(true)
+    //          }
+
+    //    })
+
+
+    // let validation = Joi.validate({
+    //     username:_user,
+    //     isbn: _isbn
+    // }, joiSchema)
+
+    // console.log(validation)
+
+    // if (validation.error === null) {
+
+    //         users.findOneAndUpdate({user:_user}, {$pull: {[checkData]: {isbn: _isbn}}}).then(data =>{
+    //             console.log(data)
+
+    //             callback(data,201)
+    //         })
+    // }else callback('Galat hai bhai Request',400)
+// }
--- bookproject/server/server.js
+++ bookproject/server/server.js
@@ -8,10 +8,10 @@ const app = express()
 app.use(bodyParser.json())
 
 
+
 mongoose.connect('mongodb://localhost/booksProject', {
   useNewUrlParser: true
 })
-
 
 
 app.get('/', (req, res) => {
@@ -19,32 +19,28 @@ app.get('/', (req, res) => {
 })
 app.get('/api/books', (req, res) => {
   books.getBooks((err, books) => {
-
+    if (err) throw (err)
     res.json(books)
   })
 })
-app.get('/api/books/:_id', (req, res) => {
-  books.getBookId(req.params._id, (err, books) => {
+// app.get('/api/books/:_id', (req, res) => {
+//   books.getBookId(req.params._id, (err, books) => {
+//     if (err) throw (new Error(""))
+//     res.json(books)
 
-    res.json(books)
-
-  })
-})
-
-app.get('/users/:_id', (req, res) => {
-  users.getUserId(req.params._id, (err, user) => {
-    res.json(user)
-  })
-})
-// app.get('/users', (req, res) => {
-//   users.getUsers((err, user) => {
-//     res.json(user)
 //   })
 // })
 
+app.get('/users', (req, res) => {
+  users.getUserId(req.get('Referer'), (user,status) => {
+   
+    res.status(status).send(user)
+  })
+})
+
 app.post('/users', (req, res) => {
-  users.addUser(req.body, (err, user) => {
-    res.json(user)
+  users.addUser(req.get('Referer'), (user,status) => {
+    res.status(status).json(user)
   })
 })
 
@@ -55,39 +51,64 @@ app.get('/api/list/:checkData', (req, res) => {
 })
 
 app.post('/api/list/:checkData', (req, res) => {
+ 
+    users.userBooksUpdate(req.get('Referer'),req.body.isbn,req.params.checkData,(user,status)=>{
+      res.status(status).send(user)
+    })
 
-  users.userBooksUpdate(req.get('Referer'), req.body.isbn, req.params.checkData, (err, user) => {
-    res.json(user)
-  })
+ // let check = req.params.checkData;
+  // let userName = req.get('Referer');
+  // let _isbn = req.body.isbn
+
+  // books.findOne({
+  //   isbn: _isbn
+  // }).then(data => {
+  //   console.log(data)
+  //   users.findOne({
+  //     user: userName
+  //   }).then(userData => {
+  //     let isIsbnExist = userData[check].some(element => element.isbn === _isbn)
+
+  //     if (!isIsbnExist) {
+  //       userData[check].push({
+  //         isbn: data.isbn
+  //         // title: data.title
+  //       })
+  //       userData.save();
+  //       res.send("Sucess")
+  //     } else {
+  //       res.status(400).send("Already present");
+  //     }
+  //   })
+
+
+  // })
+
 })
 
-app.delete('/api/list/:checkData/:_id', (req, res) => {
 
-  users.userBooksDelete(req.params._id, req.body.isbn, req.params.checkData, (err, user) => {
-    res.json(user)
-  })
+app.delete('/api/list/:checkData/:_isbn', async (req, res) => {
+  try{   
+   let result= await users.userBooksDelete(req.get('Referer'),req.params._isbn,req.params.checkData)
+
+    // if (user) {
+    //   res.send("deleted")
+    // } else {
+    //   res.send("not Exist")
+    // }
+
+  if(result==='Galat hai bhai Request'){
+       res.status(400).send('Galat hai bhai Request');
+     }else if (result==="don't exist"){
+       res.status(400).send("don't exist")
+     }else res.status(200).send(result)
+    }catch(error){
+      res.status(400).send(error)
+    }
+
 })
+
 
 app.listen(3000, function () {
   console.log('server started on port 3000....')
-})
-
-
-
-
-
-//  GET /books/:id
-
-// List
-
-// GET /list/want-to-read
-// POST  /list/want-to-read
-// DELETE  /list/want-to-read/:id
-
-// GET /list/reading
-// POST  /list/reading
-// DELETE  /list/reading/:id
-
-// GET /list/read
-// POST  /list/read
-// DELETE  /list/read/:id
+});
